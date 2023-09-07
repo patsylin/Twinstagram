5da7e7945b85850c66a4005e6676532f15389623
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-09-07 13:52:51 -0500

    updates

--- instaGram/server/index.js
+++ instaGram/server/index.js
@@ -1,8 +1,37 @@
-const cors = require('cors');
-app.use(cors());
-
+// server/index.js
+const express = require('express');
+const cors = require('cors'); // Import the CORS middleware
+const app = express();
 const client = require('./db/client');
-client.connect();
 
-//Router: /api
-//app.use('/api', require('./api'));
+app.use(cors()); // Enable CORS for all routes
+
+
+// Base route
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// API router with /api prefix
+const apiRouter = express.Router();
+
+apiRouter.get('/greet', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
+
+app.use('/api', apiRouter);
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to the PostgreSQL database
+client.connect()
+  .then(() => {
+    console.log('Connected to the PostgreSQL database');
+    // Start the server once the database is connected
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to the database:', err);
+  });
