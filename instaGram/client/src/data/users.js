42982449513e74cb42ac31f21d62297f20b062c8
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2025-09-24 11:03:48 -0700

    updated user names

---
+++ instaGram/client/src/data/users.js
@@ -0,0 +1,7 @@
+export const USERS = [
+  { handle: "penny_pumpkin", name: "Penny O'Brien" },
+  { handle: "NatureGirl", name: "Sally Soleman" },
+  { handle: "YogiForever", name: "Willow Wilson" },
+  { handle: "BeYourself", name: "Oscar Wilding" },
+  { handle: "ConnectedToLife ", name: "Allie Anderson" },
+];
